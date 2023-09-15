7d573f51bd5bde74d861089b636d410927a04910
murilocarolino <123740523+murilocarolino@users.noreply.github.com>
2023-09-15 17:17:12 -0300

    Update aula03.js

--- aula03.js
+++ aula03.js
@@ -1,57 +1,69 @@
-'use script'
+'use strict'
 
-const botaoSomar = document.getElementById('somar')
-const botaoIdentificar = document.getElementById('identificar')
-const botaoMedia = document.getElementById('media')
+const botaoSomar = document.getElementById('SOMAR')
+const botaoIndentificar = document.getElementById('indentificar')
 
 function somar () {
-    const numero1 = Number (document.getElementById('numero1').value)
-    const numero2 = Number (document.getElementById('numero2').value)
-    const resultado = document.getElementById('resultado')
+    const numero1 = Number(document.getElementById('numero1').value)
+    const numero2 = Number(document.getElementById('numero2').value)
+    const resultado1 = document.getElementById('resultado1')
 
     const total = numero1 + numero2
 
-    resultado.textContent = total
+    resultado1.textContent = total
 }
 
-function identificar () {
+function indentificar () {
     const numero = Number(document.getElementById('numero-item2').value)
     const resultado = document.getElementById('resultado-item2')
-
-    if (numero > 0) {
-        resultado.textContent = 'POSITIVO'
-    } else if (numero < 0) {
+    if (numero > 0 ){
+        resultado.textContent = 'POSITIVO'  
+    } else if (numero < 0 ){
         resultado.textContent = 'NEGATIVO'
-    } else {
+    }else{
         resultado.textContent = 'ZERO'
     }
 }
 
-function media () {
-    const nota1 = Number(document.getElementById('nota1').value)
-    const nota2 = Number(document.getElementById('nota2').value)
-    const nota3 = Number(document.getElementById('nota3').value)
-    const nota4 = Number(document.getElementById('nota4').value)
-
-    const md1 = (nota1 + nota2 + nota3 + nota4)/4
-
-    if (media >= 7) {
-        alert("Aprovado!! sua média é: " + md1)
-    } else (media < 6.99) {
-        const ne = prompt ('A sua média ${md1}. Nota de exame: ')
-
-        const md2 = (md1 + Number(ne))/2
-
-        if (md2 > 5.9) {
-            alert('Sua média agora é ${md2}. Aprovado no exame1')
+function calcularMedia() {
+    const nota1 = parseFloat(document.getElementById("nota1").value);
+    const nota2 = parseFloat(document.getElementById("nota2").value);
+    const nota3 = parseFloat(document.getElementById("nota3").value);
+    const nota4 = parseFloat(document.getElementById("nota4").value);
+    
+    const media1 = (nota1 + nota2 + nota3 + nota4) / 4;
+    const resultadoDiv = document.getElementById("resultado-d");
+    
+    if (media1 >= 7) {
+        resultadoDiv.textContent = "Aprovado";
+    } else {
+        const notaExame = parseFloat(document.getElementById("nota-exame").value);
+        const media2 = (media1 + notaExame) / 2;
+        
+        if (media2 >= 5) {
+            resultadoDiv.textContent = "Aprovado em exame";
         } else {
-            alert('Sua média agora é ${md2}. Você foi reprovado.')
+            resultadoDiv.textContent = "Reprovado";
+        }
+    }
+    
+    resultadoDiv.textContent += " - Média: " + media1.toFixed(2);
+    
+}
+function verificarParImpar() {
+    const numero = parseInt(document.getElementById("numero-item2").value);
+    const resultadoDiv = document.getElementById("resultado-item2");
+    
+    if (isNaN(numero)) {
+        resultadoDiv.textContent = "Digite um valor numérico.";
+    } else {
+        if (numero % 2 === 0) {
+            resultadoDiv.textContent = numero + " é um número par.";
+        } else {
+            resultadoDiv.textContent = numero + " é um número ímpar.";
         }
     }
 }
 
 botaoSomar.addEventListener('click', somar)
-
-botaoIdentificar.addEventListener('click', identificar)
-
-botaoMedia.addEventListener('click', media)
+botaoIndentificar.addEventListener('click', indentificar)
